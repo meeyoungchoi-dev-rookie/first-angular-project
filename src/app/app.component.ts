c090dc6467a64fbcd1d5e0015f01014316ebd837
meeyoungchoi <goodlife1359@gmail.com>
2022-08-04 22:43:25 +0900

    Angular 프로젝트 생성

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// component 클래스
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'Hello Angular!!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Angular 모듈을 적용한 클래스
+// Angualr의 root 모듈을 설정하는 클래스
 @NgModule({
   declarations: [
     AppComponent
